914c7b8e6233a4d275816b336c32ce6c9946bc2c
sunwei <me@sunwei.xyz>
2024-10-22 19:43:03 +0800

    remove: duplicated themes

--- bkstatic/manualofme-quark.js
+++
@@ -1,9 +0,0 @@
-$(window).ready(function() {
-    $(".slideto").click(function(e) {
-        e.preventDefault();
-        var aid = $(this).attr("href");
-        $('html,body').animate({scrollTop: $(aid).offset().top},1000);
-    });
-    
-    $('.ephemeral').delay(5000).slideUp();
-});
